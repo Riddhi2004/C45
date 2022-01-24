eabd12df73ae96c403bc0d62a07a621a080e5f84
Riddhi2004 <75730830+Riddhi2004@users.noreply.github.com>
2022-01-24 19:43:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombies, zombieImg;
+var bullets, bulletImg;
+var lives, liveImg;
+
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+  zombieImg = loadImage("assets/zombie.png");
+  bulletImgs = loadImage("assets/bullet.png");
+
+  liveImg = loadImage("assets/heart_3.png");
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+   zombies = createSprite(1200,300, 70, 70);
+   zombies.addImage(zombieImg)
+   zombies.scale = 0.2
+   zombies.velocityX=(-4);
+
+   lives = createSprite(1250,30,60,60);
+   lives.addImage(liveImg);
+   lives.scale=0.2;
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+  
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+
+drawSprites();
+
+}
